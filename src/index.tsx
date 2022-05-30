c393df7490a84830056b8069e9f0260430635fee
Milene Toazza <mitoe@live.com>
2022-05-30 00:44:26 -0300

    style: format files

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
-import Home from './home';
-import Products from './products';
+import Home from './home'
+import Products from './products'
 
-import { Header, Footer } from './common';
+import { Header, Footer } from './common'
 
-import './main.scss';
+import './main.scss'
 
 class Square extends React.Component {
   render() {
@@ -20,8 +15,7 @@ class Square extends React.Component {
       <main>
         <Header />
         <nav>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="produtos">Produtos</Link>
+          <Link to="/">Home</Link> | <Link to="produtos">Produtos</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -29,13 +23,13 @@ class Square extends React.Component {
         </Routes>
         <Footer />
       </main>
-    );
+    )
   }
 }
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <Square />
   </BrowserRouter>
-);
+)
--- src/products/index.tsx
+++ src/products/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 export default function Products() {
   return (
     <main style={{ padding: '1rem 0' }}>
       <h2>Produtos</h2>
     </main>
-  );
+  )
 }
